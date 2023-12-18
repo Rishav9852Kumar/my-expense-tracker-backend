fba0127d45d39b53a599255c92fad406b25c5ff5
Rishav <rishav.btech.cs19@iiitranchi.ac.in>
2023-12-19 01:37:03 +0530

    add and delete expense api completed

---
+++ src/event/expenseEvent.js
@@ -0,0 +1,133 @@
+import { connect } from '@planetscale/database';
+
+async function handleExpenseRequest(request, env) {
+	const config = {
+		host: env.DATABASE_HOST,
+		username: env.DATABASE_USERNAME,
+		password: env.DATABASE_PASSWORD,
+		fetch: (url, init) => {
+			delete init['cache'];
+			return fetch(url, init);
+		},
+	};
+	const conn = connect(config);
+
+	switch (request.method) {
+		case 'GET':
+			return handleDeleteExpense(request, conn);
+		case 'POST':
+			return handlePostExpenseEntry(request, conn);
+		default:
+			return new Response('Invalid request method', {
+				headers: { 'content-type': 'text/plain' },
+				status: 400, // Bad Request
+			});
+	}
+    async function handlePostExpenseEntry(request, conn) {
+			try {
+				const url = new URL(request.url);
+				const expense_title = url.searchParams.get('expense_title');
+				const expense_category = url.searchParams.get('expense_category');
+				const expense_amount = parseInt(url.searchParams.get('expense_amount'));
+				const expense_desc = url.searchParams.get('expense_desc');
+				const star_marked = url.searchParams.get('star_marked').toLowerCase() === 'true';
+
+				const expense_creation_date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+				const insertResult = await conn.execute(
+					'INSERT INTO ExpenseEntry (expense_title, expense_creation_date, expense_category, expense_amount, expense_desc, star_marked) VALUES (?, ?, ?, ?, ?, ?);',
+					[expense_title, expense_creation_date, expense_category, expense_amount, expense_desc, star_marked]
+				);
+
+				if (insertResult.error) {
+					return new Response(insertResult.error, {
+						headers: {
+							'content-type': 'text/plain',
+							'Access-Control-Allow-Origin': '*',
+							'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+						},
+						status: 500, // Internal Server Error
+					});
+				}
+
+				return new Response('Expense entry was added successfully', {
+					status: 200,
+					headers: {
+						'content-type': 'text/plain',
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+					},
+				});
+			} catch (error) {
+				return new Response(error + '\n' + request, {
+					headers: {
+						'content-type': 'text/plain',
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+					},
+					status: 500, // Internal Server Error
+				});
+			}
+		}
+		async function handleDeleteExpense(request, conn) {
+			try {
+				const url = new URL(request.url);
+				const expense_id = url.searchParams.get('expense_id'); // Get the id from URL parameters
+
+				if (!expense_id) {
+					return new Response('Missing Expense Entry id', {
+						headers: {
+							'content-type': 'text/plain',
+							'Access-Control-Allow-Origin': '*',
+							'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+						},
+						status: 400, // Bad Request
+					});
+				}
+
+				const deleteResult = await conn.execute('DELETE FROM ExpenseEntry WHERE expense_id = ?;', [expense_id]);
+
+				if (deleteResult.error) {
+					return new Response(deleteResult.error, {
+						headers: {
+							'content-type': 'text/plain',
+							'Access-Control-Allow-Origin': '*',
+							'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+						},
+						status: 500, // Internal Server Error
+					});
+				}
+
+				if (deleteResult.affectedRows === 0) {
+					return new Response('No Expense Entry found with id: ' + id, {
+						headers: {
+							'content-type': 'text/plain',
+							'Access-Control-Allow-Origin': '*',
+							'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+						},
+						status: 404, // Not Found
+					});
+				}
+
+				return new Response('Expense Entry was deleted successfully', {
+					status: 200,
+					headers: {
+						'content-type': 'text/plain',
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+					},
+				});
+			} catch (error) {
+				return new Response(error + '\n' + request, {
+					headers: {
+						'content-type': 'text/plain',
+						'Access-Control-Allow-Origin': '*',
+						'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+					},
+					status: 500, // Internal Server Error
+				});
+			}
+		}
+}
+
+export default handleExpenseRequest;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'itty-router';
 import handleRequest from './accounts/user.js';
+import handleExpenseRequest from './event/expenseEvent.js';
 
 // Router
 const router = Router();
@@ -8,14 +9,18 @@ const router = Router();
 router.get('/user', (request, env) => handleRequest(request, env));
 router.post('/user', (request, env) => handleRequest(request, env));
 
+// Add Expenses Event Routes
+router.get('/event', (request, env) => handleExpenseRequest(request, env));
+router.post('/event', (request, env) => handleExpenseRequest(request, env));
+
 export default {
 	async fetch(request, env, ctx) {
-			const result = await router.handle(request, env, ctx);
+		const result = await router.handle(request, env, ctx);
 
-			if (!result) {
-				return new Response('Invalid URL', { status: 404 });
-			}
+		if (!result) {
+			return new Response('Invalid URL', { status: 404 });
+		}
 
-			return result;
+		return result;
 	},
 };
